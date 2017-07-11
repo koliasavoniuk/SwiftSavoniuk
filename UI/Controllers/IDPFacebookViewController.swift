99b1e7a0365d76081c4c0a935bad324ab302fe4f
Student002 <koliakrivbass97@mail.ru>
2017-07-11 10:39:57 +0300

    Refactoring

--- UI/Controllers/IDPFacebookViewController.swift
+++ UI/Controllers/IDPFacebookViewController.swift
@@ -14,36 +14,22 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 
 class IDPFacebookViewController: UIViewController {
-
-    let myLoginButton = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends ])
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addCustomButton()
+        
         if AccessToken.current != nil {
             print (getFriends(funcP: fetchProfile))
         }
-        
-        //prepareLoginButton()
-        addCustomButton()
-    
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    func prepareLoginButton () {
-        let loginButton = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends ])
-        loginButton.center = view.center
-        
-        view.addSubview(loginButton)
-    }
-    
     func addCustomButton() {
-        // Add a custom login button to your app
         let myLoginButton = UIButton(type: .custom)
         myLoginButton.backgroundColor = UIColor.darkGray
         myLoginButton.frame = CGRect(x: 0, y: 0, width: 180, height: 40);
@@ -51,22 +37,20 @@ class IDPFacebookViewController: UIViewController {
         myLoginButton.center = view.center;
         myLoginButton.setTitle("My Login Button", for: .normal)
         
-        // Handle clicks on the button
         myLoginButton.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
         
-        // Add the button to the view
         view.addSubview(myLoginButton)
     }
     
     @objc func loginButtonClicked() {
         let loginManager = LoginManager()
-        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+        loginManager.logIn([ .publicProfile, .email, .userFriends ], viewController: self) { loginResult in
             switch loginResult {
             case .failed(let error):
                 print(error)
             case .cancelled:
                 print("User cancelled login.")
-            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+            case .success( _, _, _):
                 let result = self.getFriends(funcP: self.fetchProfile)
                 print(result)
             }
@@ -76,28 +60,18 @@ class IDPFacebookViewController: UIViewController {
     public func getFriends(funcP: @escaping (Any?)->Void) -> Any {
         let fbRequestFriends: FBSDKGraphRequest = FBSDKGraphRequest(
             graphPath:"me/friends",
-            parameters:["fields": "id,name,about,gender,birthday,email,picture,friends.limit(100){picture,name}"])
+            parameters:["fields": "id,name,about,gender,birthday,email,picture,friends{picture,name}"])
         
         fbRequestFriends.start { (connection, result, error) in
             if error == nil && result != nil {
                 funcP(result)
             } else {
-                print("Error +++++++++++++++++++++++++++\(String(describing: error))")
+                print("Error\(String(describing: error))")
             }
         }
         return "getFriends"
     }
-/*
-    func fetchProfile(result:Any?) {
-        
-        print("fetch profile-----------------------------------------------------------------")
-        
-        let dic = result as! NSDictionary
 
-        print("Request Friends result : \(dic)")
-    }
-*/
-    
     func fetchProfile(result:Any?) {
         
         print("fetch profile")
