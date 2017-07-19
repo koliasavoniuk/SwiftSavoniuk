1fc3527b8a6d317d7e30a58615ffd8cc298129a3
Student002 <koliakrivbass97@mail.ru>
2017-07-19 12:51:22 +0300

    Swift Task 5 -> asses IDPFriendsDetailViewController file

---
+++ Source/UI/Controllers/FriendsDetailViewController/IDPFriendsDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  IDPFriendsDetailViewController.swift
+//  IDPSwiftSavoniuk
+//
+//  Created by Student002 on 7/19/17.
+//  Copyright © 2017 Student002. All rights reserved.
+//
+
+import UIKit
+
+class IDPFriendsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
